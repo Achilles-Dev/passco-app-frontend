d62144bf6a811b65efc3c5b5c4f03df795c56df4
Achilles-Dev <solohag92@gmail.com>
2022-10-11 10:06:43 +0000

    Add QuestionsList

---
+++ src/features/questions/QuestionsList.js
@@ -0,0 +1,86 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
+import { Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchQuestions, selectAllQuestions } from './questionsSlice';
+
+const optionLetters = ['A', 'B', 'C', 'D'];
+
+const QuestionsList = ({ auth }) => {
+  const id = auth.ids[0];
+  const questions = useSelector(selectAllQuestions);
+  const dispatch = useDispatch();
+  const { subjectId, year } = useParams();
+
+  useEffect(() => {
+    if (auth.entities[id]) {
+      const { token } = auth.entities[id];
+      dispatch(fetchQuestions({ token, year, subjectId }));
+    }
+  }, []);
+
+  const initialValues = {
+    option: '',
+  };
+
+  const validationSchema = Yup.object({
+
+  });
+
+  const handleSubmit = () => {
+
+  };
+
+  return (
+    <div>
+      <h2>
+        Questions for the year,
+        {year}
+      </h2>
+      <div>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={(values) => {
+            handleSubmit(values);
+          }}
+        >
+          <Form>
+            { questions.map((question, index) => (
+              <div key={question.id}>
+                <h3>
+                  {index}
+                  +1
+                  {question.content}
+                </h3>
+                {
+                  question.options.map((option, index) => (
+                    <div key={option}>
+                      <label htmlFor={`${option}${index}`}>
+                        {optionLetters[index]}
+                      </label>
+                      <Field
+                        name={`${option}${index}`}
+                        type="radio"
+                      >
+                        {option}
+                      </Field>
+                    </div>
+                  ))
+                }
+              </div>
+            ))}
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
+};
+
+QuestionsList.propTypes = {
+  auth: PropTypes.func.isRequired,
+};
+
+export default QuestionsList;
